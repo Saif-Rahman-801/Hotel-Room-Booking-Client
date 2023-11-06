e497fdccfa551709c7b3054170b7ea0d0b72dd68
Saif-Rahman-801 <saifurs102@gmail.com>
2023-11-06 13:57:44 +0600

    aos implemented in the images

--- src/Components/Home/HomeImg.jsx
+++ src/Components/Home/HomeImg.jsx
@@ -10,7 +10,10 @@ const HomeImg = () => {
   }, []);
   return (
     <div className="my-5">
-      <div className="grid grid-cols-2 gap-4">
+        <div className="text-center text-3xl font-bold my-5">
+            <h2>EXPLORE OUR ROOMS VIEW</h2>
+        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {imgData.map((img) => (
           <HomeImgCard key={img._id} img={img} />
         ))}
--- src/Components/Home/HomeImgCard.jsx
+++ src/Components/Home/HomeImgCard.jsx
@@ -1,8 +1,17 @@
 import PropTypes from "prop-types";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 const HomeImgCard = ({ img }) => {
+  useEffect(() => {
+    AOS.init({
+      // Global settings here (optional)
+    });
+  }, []);
+
   return (
-    <div>
-      <img src={img.url} className="w-full h-[500px] rounded-md" />
+    <div data-aos="fade-up">
+      <img src={img.url} className="w-full h-full md:h-[500px] rounded-md" />
     </div>
   );
 };
