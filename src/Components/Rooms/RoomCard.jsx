71b6e25cd3bd6a684550c147368e1de07aee3090
Saif-Rahman-801 <saifurs102@gmail.com>
2023-11-07 16:52:29 +0600

    implemented filter system by price in the rooms page

--- src/Components/Rooms/RoomCard.jsx
+++ src/Components/Rooms/RoomCard.jsx
@@ -2,22 +2,28 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 const RoomCard = ({ data }) => {
   return (
-    <Link to="/roomDetails">
-    <div className="my-5">
-      <div className="card h-[400px] bg-base-100 shadow-xl">
-        <figure>
-          <img src={data.roomImage} alt="room" />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title text-2xl">{data.roomType} </h2>
-          <h2 className="text-sm font-bold">Ratings: <span className="text-orange-600"> {data.reviews}</span> </h2>
-          <p className="text-sm text-gray-600 font-medium">
-            Price: {data.pricePerNight}$ Per night
-          </p>
+    <div>
+     
+      <Link to="/roomDetails">
+        <div className="my-5">
+          <div className="card h-[400px] bg-base-100 shadow-xl">
+            <figure>
+              <img src={data.roomImage} alt="room" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title text-2xl">{data.roomType} </h2>
+              <h2 className="text-sm font-bold">
+                Ratings:{" "}
+                <span className="text-orange-600"> {data.reviews}</span>{" "}
+              </h2>
+              <p className="text-sm text-gray-600 font-medium">
+                Price: {data.pricePerNight}$ Per night
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
-    </Link>
   );
 };
 
--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -1,11 +1,49 @@
 import { useLoaderData } from "react-router-dom";
 import RoomCard from "./RoomCard";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const Rooms = () => {
-  const roomData = useLoaderData();
+  const data = useLoaderData();
+  const [roomData, setRoomData] = useState(data);
+  const handleFilter = (e) => {
+    e.preventDefault();
+    const price = e.target.price.value;
+    if (price < 80 || price > 250) {
+      toast.error("Please put your price between 80$ to 250$");
+      return;
+    }
+
+    const filteredData = roomData.filter((data) => data.pricePerNight <= price);
+    // console.log(filteredData);
+    setRoomData(filteredData);
+    e.target.reset();
+  };
 
   return (
-    <div >
+    <div>
+      <form
+        onSubmit={handleFilter}
+        className="flex items-center justify-center space-x-4 my-5"
+      >
+        <input
+          type="number"
+          name="price"
+          className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          placeholder="Enter price to filter"
+        />
+        <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 font-medium rounded-md transition duration-300">
+          Filter
+        </button>
+      </form>
+      <div className="flex justify-center">
+        <button
+          onClick={() => setRoomData(data)}
+          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 font-medium rounded-md transition duration-300"
+        >
+          See All
+        </button>
+      </div>
       <div className="grid grid-cols-3 gap-6 ">
         {roomData.map((data) => (
           <RoomCard data={data} key={data.id} />
